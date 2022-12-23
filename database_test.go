754a890b2bb4b105fa3908e4a8f4e2ec47b32819
Arif Rachman Hakim <arifrachman149@gmail.com>
2022-12-23 17:35:25 +0700

    Add function Open and function SetConn

--- database_test.go
+++ database_test.go
@@ -3,18 +3,37 @@ package godatabases
 import (
 	"database/sql"
 	"testing"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+func Open() {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go-databases")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
+
+func SetConn() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go-databases")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
 
 func TestDatabases(t *testing.T) {
 
 }
 
 func TestOpenConn(t *testing.T) {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go-databases")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+	Open()
 }
