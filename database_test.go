4a75102094510fb940a27e7df78cd59cc695e92c
Arif Rachman Hakim <arifrachman149@gmail.com>
2022-12-23 17:22:58 +0700

    Add function TestOpenConn

--- database_test.go
+++ database_test.go
@@ -1,6 +1,7 @@
 package godatabases
 
 import (
+	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -8,4 +9,12 @@ import (
 
 func TestDatabases(t *testing.T) {
 
+}
+
+func TestOpenConn(t *testing.T) {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go-databases")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
